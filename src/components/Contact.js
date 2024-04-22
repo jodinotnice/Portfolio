6c0044bd3086c0275bd99a179ab2a820659ecef4
jodinotnice <jordan.akpovi@gmail.com>
2024-04-22 20:29:59 +0200

    affichages de images uniquement en ligne

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,6 @@ export default function Contact() {
   }
 
   return (
-    <></> /*
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex justify-center items-center flex sm::flex-nowrap flex-wrap">
         <form
@@ -77,6 +76,6 @@ export default function Contact() {
           </button>
         </form>
       </div>
-    </section>*/
+    </section>
   );
 }
